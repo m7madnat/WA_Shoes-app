8535abe64d5fd56362538d244a01111ce481a81a
m7madnat <m7madnatseh1999@gmail.com>
2022-11-19 17:53:03 +0200

    Update Input.js

--- src/components/AddProduct/Input.js
+++ src/components/AddProduct/Input.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from "react";
 const Input = ({id, type, holder, value, callBack, submitted}) => {
+	// eslint-disable-next-line no-unused-vars
     const [state, setState] = useState({value: ""});
 
     const handleChange = ({target}) => {
